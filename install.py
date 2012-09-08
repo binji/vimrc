3ac268005d15312dc0b033995f53ca42d3012433
Ben Smith <binjimin@gmail.com>
2012-09-08 14:12:42 -0700

    add windows support

--- install.py
+++ install.py
@@ -2,14 +2,25 @@
 import os
 import shutil
 import subprocess
+import sys
 
 exists = os.path.exists
 join = os.path.join
-HOME = os.environ.get('HOME')
+getenv = os.environ.get
+
+if sys.platform == 'win32':
+  HOME = join(getenv('HOMEDRIVE'), getenv('HOMEPATH'))
+  use_shell = True
+else:
+  HOME = getenv('HOME')
+  use_shell = False
+
 VUNDLE_GITHUB_URL = 'https://github.com/gmarik/vundle.git'
 VUNDLE_DIR = join(HOME, '.vim', 'bundle', 'vundle')
+
 if not exists(VUNDLE_DIR):
-  subprocess.call(['git', 'clone', VUNDLE_GITHUB_URL, VUNDLE_DIR])
+  os.makedirs(VUNDLE_DIR)
+  subprocess.call(['git', 'clone', VUNDLE_GITHUB_URL, VUNDLE_DIR], shell=use_shell)
 else:
-  subprocess.call(['git', 'pull'], cwd=VUNDLE_DIR)
-shutil.copy('.vimrc', join(os.environ.get('HOME'), '.vimrc'))
+  subprocess.call(['git', 'pull'], cwd=VUNDLE_DIR, shell=use_shell)
+shutil.copy('.vimrc', join(HOME, '.vimrc'))
